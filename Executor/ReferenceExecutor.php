1501383fd3adaa34d8a2a29ab94abbfecd1a917c
Daniel Sloof <daniel@rubic.nl>
2022-07-21 15:40:31 +0200

    feat: allow executor plugin to work with older webonyx graphql versions (#2)

---
+++ Executor/ReferenceExecutor.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace Graycore\GraphQlIntrospectionCache\Executor;
+
+use GraphQL\Executor\ExecutionContext;
+use GraphQL\Executor\ExecutionResult;
+use GraphQL\Executor\ExecutorImplementation;
+use GraphQL\Executor\Promise\Promise;
+use GraphQL\Executor\Promise\PromiseAdapter;
+use GraphQL\Language\AST\DocumentNode;
+use GraphQL\Type\Schema;
+use GraphQL\Utils\Utils;
+use Magento\Framework\App\ObjectManager;
+use ReflectionMethod;
+use ReflectionProperty;
+use SplObjectStorage;
+
+class ReferenceExecutor extends \GraphQL\Executor\ReferenceExecutor
+{
+    private static ?self $executorInstance = null;
+
+    protected function __construct(ExecutionContext $context)
+    {
+        if (is_callable('parent::__construct')) {
+            parent::__construct($context);
+        } else {
+            $this->setExecutorPrivateProp('UNDEFINED', Utils::undefined(), true);
+            $this->setExecutorPrivateProp('exeContext', $context);
+            $this->setExecutorPrivateProp('subFieldCache', new SplObjectStorage());
+        }
+    }
+
+    private function setExecutorPrivateProp(string $property, $value, bool $static = false): void
+    {
+        try {
+            $reflectionProperty = new ReflectionProperty(\GraphQL\Executor\ReferenceExecutor::class, $property);
+            $reflectionProperty->setAccessible(true);
+            $reflectionProperty->setValue($static ? null : $this, $value);
+        } catch (\ReflectionException $e) {}
+    }
+
+    public static function create(
+        PromiseAdapter $promiseAdapter,
+        Schema $schema,
+        DocumentNode $documentNode,
+        $rootValue,
+        $contextValue,
+        $variableValues,
+        ?string $operationName,
+        callable $fieldResolver
+    ) : ExecutorImplementation {
+        if (self::$executorInstance !== null) {
+            return self::$executorInstance;
+        }
+
+        $reflectionMethod = new ReflectionMethod(\GraphQL\Executor\ReferenceExecutor::class, 'buildExecutionContext');
+        if ($reflectionMethod->isPrivate()) {
+            $reflectionMethod->setAccessible(true);
+        }
+
+        $exeContext = $reflectionMethod->invoke(
+            null,
+            $schema,
+            $documentNode,
+            $rootValue,
+            $contextValue,
+            $variableValues,
+            $operationName,
+            $fieldResolver,
+            $promiseAdapter
+        );
+
+        if (is_array($exeContext)) {
+            $promise = $promiseAdapter->createFulfilled(new ExecutionResult(null, $exeContext));
+            return new class($promise) implements ExecutorImplementation
+            {
+                private Promise $result;
+
+                public function __construct(Promise $result)
+                {
+                    $this->result = $result;
+                }
+
+                public function doExecute() : Promise
+                {
+                    return $this->result;
+                }
+            };
+        }
+
+        return self::$executorInstance = ObjectManager::getInstance()->create(
+            ReferenceExecutor::class,
+            ['context' => $exeContext]
+        );
+    }
+}
--- Plugin/ExecutorPlugin.php
+++ Plugin/ExecutorPlugin.php
@@ -2,21 +2,12 @@
 
 namespace Graycore\GraphQlIntrospectionCache\Plugin;
 
-use Closure;
 use GraphQL\Executor\Executor;
-use GraphQL\Executor\ReferenceExecutor;
+use Graycore\GraphQlIntrospectionCache\Executor\ReferenceExecutor;
 use Magento\Framework\GraphQl\Query\QueryProcessor;
-use Magento\Framework\ObjectManagerInterface;
 
 class ExecutorPlugin
 {
-    private ObjectManagerInterface $objectManager;
-
-    public function __construct(ObjectManagerInterface $objectManager)
-    {
-        $this->objectManager = $objectManager;
-    }
-
     /**
      * Before processing a GraphQL query, replace the factory with a wrapper that uses the object manager.
      * This allows us to create plugins on the ReferenceExecutor.
@@ -25,36 +16,7 @@ class ExecutorPlugin
         QueryProcessor $subject,
         ...$args
     ): array {
-        Executor::setImplementationFactory(
-            fn(
-                $promiseAdapter,
-                $schema,
-                $documentNode,
-                $rootValue,
-                $contextValue,
-                $variableValues,
-                $operationName,
-                $fieldResolver
-            ) => $this->objectManager->create(
-                ReferenceExecutor::class,
-                [
-                    'context' => Closure::bind(
-                        fn() => ReferenceExecutor::buildExecutionContext(
-                            $schema,
-                            $documentNode,
-                            $rootValue,
-                            $contextValue,
-                            $variableValues,
-                            $operationName,
-                            $fieldResolver,
-                            $promiseAdapter
-                        ),
-                        null,
-                        ReferenceExecutor::class
-                    )()
-                ]
-            )
-        );
+        Executor::setImplementationFactory([ReferenceExecutor::class, 'create']);
         return $args;
     }
 }
