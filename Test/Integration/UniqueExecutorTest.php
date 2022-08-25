9daede31b3de7cab58973bdb54ed9a636560bd01
Daniel Sloof <daniel@rubic.nl>
2022-08-25 22:18:29 +0200

    fix: ensure reference executor always returns a new object (#6)

---
+++ Test/Integration/UniqueExecutorTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Graycore\GraphQlIntrospectionCache\Test\Integration;
+
+use GraphQL\Executor\Promise\Adapter\SyncPromiseAdapter;
+use GraphQL\Language\AST\DocumentNode;
+use GraphQL\Type\Schema;
+use Graycore\GraphQlIntrospectionCache\Executor\ReferenceExecutor;
+use Magento\TestFramework\ObjectManager;
+use PHPUnit\Framework\TestCase;
+
+class UniqueExecutorTest extends TestCase
+{
+    public function testThatCreatedExecutorsAreUnique()
+    {
+        $om = ObjectManager::getInstance();
+
+        $promiseAdapter = $om->create(SyncPromiseAdapter::class);
+        $schema = $om->create(Schema::class, ['config' => []]);
+        $documentNode = $om->create(DocumentNode::class, ['vars' => []]);
+        $documentNode->definitions = [];
+
+        $first = ReferenceExecutor::create(
+            $promiseAdapter,
+            $schema,
+            $documentNode,
+            null,
+            null,
+            [],
+            null,
+            function() {}
+        );
+
+        $second = ReferenceExecutor::create(
+            $promiseAdapter,
+            $schema,
+            $documentNode,
+            null,
+            null,
+            [],
+            null,
+            function() {}
+        );
+
+        $this->assertNotEquals(spl_object_id($first), spl_object_id($second));
+    }
+}
